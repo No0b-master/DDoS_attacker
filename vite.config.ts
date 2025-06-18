de368bfa8bf362c0b682a590b61dbabbff0f0993
Mohd-Ahmad-007 <mohd.ahmad@escortskubota.com>
2025-06-18 18:02:54 +0530

    asndaskldnaskdl

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-    base: "/DDoS_attacker",
-  plugins: [
-    react()
-  ].filter(Boolean),
+export default defineConfig({
+  base: '/DDoS_attacker/', // ← THIS IS CRITICAL
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+})
